c2f219f153aee2585afc51bb7e6d1b79aa567149
omatie <omatie.persad2004@gmail.com>
2025-02-19 16:29:56 -0400

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,167 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
 
 for(let i=0; i< arr.length; i++){
   console.log(arr[i])
 }
+let radius = 7;
+const pi = 3.14;
+
+let area = radius * radius * pi;
+let name = "bob";
+let age = 24;
+
+console.log(typeof(name))//string
+console.log(typeof(age))//number
+
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+//Hello my name is bob, I'm 24 years old.
+
+console.log(`I was born in ${2020 - age}`)
+//I was born in 1966
+
+for(let i=0; i< 50; i++){
+    if(i %3 === 0 && i%5 === 0){ // is divisible by 3 and 5?
+      console.log('fizzbuzz');
+    }else if(i%3 === 0){ // ok it's not divisible by both how about just 3?
+      console.log('fizz');
+    }else if(i%5 === 0){ //no? what about just 5 then?
+      console.log('buzz');
+    }else{              //so its not divisible by 3 or 5 then
+      console.log(i);
+    }
+  }
+
+  let nowTimestamp = Date.now();//store the current Date
+
+console.log(nowTimestamp);//unix timestamp in milliseconds 1581291269848
+
+let now = new Date(nowTimestamp);//today's Date object 
+
+//months go from 0 - 11
+let date = new Date(2019, 11, 17, 3, 24, 0);//December 17, 2019 03:24:00
+//printing the date
+
+console.log(date.toLocaleDateString("en-US"));//12/17/2019
+//1 is added to month because January is 0
+console.log(` ${now.getDate()} - ${now.getMonth() + 1} - ${now.getFullYear()} `); // 9 - 2 - 2020 
+
+//Calculate difference between two dates in milliseconds
+let difference = now - date;
+//convert milliseconds to days
+
+let daysBetween = difference * 1.15741e-8;
+console.log(daysBetween);
+
+function happyPrint(string){
+    console.log("😀: "+string);
+   }
+   
+   function sadPrint(string){
+    console.log("😢: "+string);
+   }
+   
+   //This high-order function adds 2 parameters and passes the answer to the callback
+   function add(a, b, callback){
+      let ans = a + b;
+      callback(ans);// call the callback and pass the answer to it
+   }
+   
+   //call add passing the callbacks to it
+   
+   add(5, 10, happyPrint);
+   add(11, 12, sadPrint);
+
+   //prints current date
+function printDate(){
+    console.log(Date().toLocaleTimeString());
+ }
+       
+ setInterval(printDate, 1000);
+
+ let arr = []//empty array
+let arr2 = [1, 2, 3, 4, 5, 6, 7]//initialize with elements
+let arr3 = [1, 12.9, "Hi", '🎈', ]// heterogeneous array
+
+console.log(arr[0]);// 1
+console.log(arr3[3]);//🎈
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+
+for(let i=0; i< arr.length; i++){
+  console.log(arr[i])
+}
+
+for (let item of arr){// for of loop
+   console.log(item);
+}
+
+let arr = [ -5, 16, 33, 42, 103, 344];
+
+console.log(arr.includes(-5));//true
+
+arr.push(11);//adds item to the end
+
+console.log(arr)//[ -5, 16, 33, 42, 103, 344, 11];
+
+let lastItem == arr.pop();//removes last item
+
+console.log(lastItem, arr);//11, [ -5, 16, 33, 42, 103, 344]
+
+arr.unshift(22);//adds item to the front
+
+console.log(arr);//[22, -5, 16, 33, 42, 103, 344]
+
+let firstItem = arr.shift();//removes first item
+console.log(firstItem, arr);//22, [-5, 16, 33, 42, 103, 344]
+
+let reversed = arr.reverse();//creates a new array in reverse order 
+console.log(reversed);//[344, 103, 42, 33, 16, -5]
+console.log(arr.join('-'));//"-5-103-16-33-344-42" joins array with provided separator
+
+let arr = [12, 33, 4, 5, -4, 8, 19, 25];
+                
+//map() creates a new array from the elements of one without changing the old one
+function double(num){
+  return num * 2;
+}
+
+let doubledArr = arr.map(double); 
+console.log(doubledArr);
+
+function isOdd(num){
+  return num%2 !== 0; 
+}
+//Filter takes a test condition and returns only the element which 
+//make the condition true
+let odds = arr.filter(isOdd);
+console.log(odds);
+
+//Returns true or false if any of the elements of the array 
+//meets a specified condition
+function has5Factor(ele){
+  return ele % 5 === 0;
+}
+
+let hasFiveFactor = arr.some(has5Factor);
+console.log(hasFiveFactor);
+
+function intCompare(a, b){
+ return a - b;
+}
+
+//sort function must return either 0, +ve, -ve
+let ascending = arr.sort(intCompare)
+console.log(ascending);
